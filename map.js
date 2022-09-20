6c2d0c68b6a9b1570397944ff532f49d65e1045c
trinadh07 <105208184+trinadh07@users.noreply.github.com>
2022-09-20 19:06:46 +0530

    practice

---
+++ map.js
@@ -0,0 +1,17 @@
+const tasks = [
+  {
+    'name'     : 'Write for Envato Tuts+',
+    'duration' : 120
+  },
+  {
+    'name'     : 'Work out',
+    'duration' : 60
+  },
+  {
+    'name'     : 'Procrastinate on Duolingo',
+    'duration' : 240
+  }
+];
+
+const task_names = tasks.map(task => task.name)
+console.log(task_names)
