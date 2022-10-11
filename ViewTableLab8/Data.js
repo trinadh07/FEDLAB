b278850e6a843ae4ae37b560f526c3ec4b7b290b
trinadh07 <105208184+trinadh07@users.noreply.github.com>
2022-10-11 22:31:40 +0530

    lab

---
+++ ViewTableLab8/Data.js
@@ -0,0 +1,47 @@
+export const products = [
+  {
+    id: 1,
+    title: "Spring and summershoes",
+    price: 20,
+    quantity: 3,
+    total: 60,
+    discountPercentage: 8.71,
+    discountedPrice: 55
+  },
+  {
+    id: 2,
+    title: "TC Reusable Silicone Magic Washing Gloves",
+    price: 29,
+    quantity: 2,
+    total: 58,
+    discountPercentage: 3.19,
+    discountedPrice: 56
+  },
+  {
+    id: 3,
+    title: "Oil Free Moisturizer 100ml",
+    price: 40,
+    quantity: 2,
+    total: 80,
+    discountPercentage: 13.1,
+    discountedPrice: 70
+  },
+  {
+    id: 4,
+    title: "Wholesale cargo lashing Belt",
+    price: 930,
+    quantity: 1,
+    total: 930,
+    discountPercentage: 17.67,
+    discountedPrice: 766
+  },
+  {
+    id: 5,
+    title: "Women Sweaters Wool",
+    price: 600,
+    quantity: 2,
+    total: 1200,
+    discountPercentage: 17.2,
+    discountedPrice: 994
+  }
+];
