2a2824ce187878090d8aa75afa63705a425a7d01
trinadh07 <105208184+trinadh07@users.noreply.github.com>
2022-10-11 23:18:15 +0530

    lab

---
+++ DigitalClock/App.js
@@ -0,0 +1,9 @@
+import DigitalClock from "./DigitalClock";
+function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <DigitalClock />
+    </div>
+  );
+}
+export default App;
